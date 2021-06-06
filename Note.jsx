51b033fcd1a88ae90aa5d6dad329270f5376de0a
SayanPal555 <85436709+SayanPal555@users.noreply.github.com>
2021-06-06 16:19:57 +0530

    Add files via upload

---
+++ Note.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div className="note">
+      <h1>Background</h1>
+      <p>
+        Myself Sayan Pal. I am 20 years old. I am studing Techno India University. I love Coding. I love Programming. 
+      </p>
+    </div>
+  );
+}
+
+export default Note;
